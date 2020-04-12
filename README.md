2cc56e49561a4229bc7c65a302a01db029c89637
ameliermd <amelie.raymond@etu.unistra.fr>
2020-04-12 22:12:48 +0200

    cannot load json

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # benchmarktool-dev
 Benchmark tool - Internship project for TPS students.
+
+## Usage
+Using a terminal in benchmarktool-dev 
+(if flash is not already installed : pip install flask)
+- python src/run.py
---
+++ src/app/views.py
@@ -0,0 +1,39 @@
+from flask import render_template, request, jsonify
+import json
+from app import app
+
+@app.route('/')
+def index():
+    json = request.get_json()
+    return render_template("index.html")
+
+
+@app.route('/data_train')
+def data_train():
+    return render_template("data_train.html")
+
+@app.route('/models')
+def models():
+    return render_template("models.html")
+
+@app.route('/results')
+def results():
+    return render_template("results.html")
+
+@app.route('/analyse', methods=['POST'])
+def analyse():
+    data = request.files['file']
+    res = data.read()
+    res = json.loads(res)
+    print(res)
+
+
+'''
+def analyse():
+    title = 'Result'
+    data = request.files['file']
+    res=data.read()
+    res = res.decode("utf-8") 
+    return render_template('/results.html')
+
+'''
