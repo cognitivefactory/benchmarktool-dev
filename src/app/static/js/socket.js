46918c10c4eec9746f632032b372e2f7255fd7f4
Amélie <amelieraym@gmail.com>
2020-10-24 21:34:03 +0200

    correction erreurs

--- src/app/static/js/socket.js
+++ src/app/static/js/socket.js
@@ -87,12 +87,21 @@ jQuery(document).ready(function ($) {
     
                         case "boolean":
                             //console.log(value + "s hould be a boolean");
-
+                            value = value.toLowerCase();
                             if(value != "true" && value != "false"){
                                 //console.log("not equal true or false");
 
                                 input_correct = false;
                                 form_correct = false;
+                            }
+                            else{
+                                if(value=="true"){
+                                    value = true;
+                                }
+                                else{
+                                    value = false;
+                                }
+                                
                             }
                         break;
     
